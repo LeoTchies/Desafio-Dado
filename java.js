1fa51f4737c3771d2e24de760e94324a75b0f0a5
LeoTchies <leo.tolotti.chies@gmail.com>
2025-07-30 18:15:23 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-document.getElementById("sorte").onclick = rodarDados;
+document.getElementById("sorte").addEventListener("click", rodarDados);
 
 function rodarDados(){
 var numeroDado1 = Math.floor((Math.random()*6)+1);
